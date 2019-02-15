e808f7c81f5714a7beaef50e2b9eded5cc3517d3
fummicc1 <fumiya989@gmail.com>
2019-02-15 11:29:42 +0900

    Orientation Done

--- lib/UIOrientation/main.dart
+++ lib/UIOrientation/main.dart
@@ -8,6 +8,23 @@ class UIOrientationPage extends StatefulWidget {
 class _UIOrientationPageState extends State<UIOrientationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("縦でも横でも幸せになれる？"),
+      ),
+      body: OrientationBuilder(builder: (context, orientation) {
+        return GridView.count(
+          crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                  "Item $index",
+                style: Theme.of(context).textTheme.headline,
+              ),
+            );
+        })
+        );
+      }),
+    );
   }
 }
